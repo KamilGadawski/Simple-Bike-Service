775f5acb096d5b3647a8f481c07981f3761ec950
kamilgadawski <s8530gtl640>
2020-12-21 19:31:09 +0100

    Added checking of fields when creating customer

--- Models/CustomerCreatingDto.cs
+++ Models/CustomerCreatingDto.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceAPI.Models
 {
     public class CustomerCreatingDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Surname { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required]
         public string TelephoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime CustomerAdd { get; set; }
     }
 }
